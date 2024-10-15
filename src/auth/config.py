3831afaf4fed272e073f7ca482e621d049ebbfbf
Danil Kyznecov <artemdancte@mail.ru>
2024-10-15 21:24:13 +0300

    start

---
+++ src/auth/config.py
@@ -0,0 +1,48 @@
+"""Файл конфигураций."""
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseServisSettings(BaseSettings):
+    """Настройки сервиса.
+
+    Args:
+        BaseSettings (class): базовые настройки.
+    """
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        extra='ignore',
+    )
+
+
+class PgSqlSettingsq(BaseServisSettings):
+    """Валидация данных для PostgreSQL.
+
+    Args:
+        BaseServisSettings (class): Настройки сервиса.
+    """
+
+    db_host: str
+    db_password: str
+    db_port: int
+    db_shema: str
+    db_name: str
+    db_user: str
+    db_driver: str
+
+
+class JwtSettings(BaseServisSettings):
+    """Валидация данных для JWT токенов.
+
+    Args:
+        BaseServisSettings (class): Настройки сервиса.
+    """
+
+    JWT_SEKRET_KEY: str
+    ALGORITM: str
+    ACCESS_TIME: int
+    REFRESH_TIME: int
+
+
+pgsqlsettings = PgSqlSettingsq()
+jwtsettings = JwtSettings()
---
+++ src/auth/main.py
@@ -0,0 +1,9 @@
+"""Точка входа."""
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000)
